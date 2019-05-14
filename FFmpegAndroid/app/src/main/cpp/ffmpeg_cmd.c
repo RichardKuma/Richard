c5b8b2711b840d3ae799118b9956b2a22200e300
RichardKuma <819941971@qq.com>
2019-05-14 17:37:00 +0800

    记录如何在so库代码中添加日志

--- FFmpegAndroid/app/src/main/cpp/ffmpeg_cmd.c
+++ FFmpegAndroid/app/src/main/cpp/ffmpeg_cmd.c
@@ -1,12 +1,20 @@
 #include <jni.h>
 #include "ffmpeg/ffmpeg.h"
 
+#include <android/log.h>
+#define TAG "FFmpegCmd"
+#define LOGI(FORMAT,...) __android_log_print(ANDROID_LOG_INFO, TAG, FORMAT, ##__VA_ARGS__);
+#define LOGE(FORMAT,...) __android_log_print(ANDROID_LOG_ERROR, TAG, FORMAT, ##__VA_ARGS__);
+
 JNIEXPORT jint JNICALL Java_com_frank_ffmpeg_FFmpegCmd_handle
 (JNIEnv *env, jclass obj, jobjectArray commands){
     int argc = (*env)->GetArrayLength(env, commands);
     char **argv = (char**)malloc(argc * sizeof(char*));
     int i;
     int result;
+
+    LOGI("%s","ffmpeg_FFmpegCmd_handle jni test log");
+
     for (i = 0; i < argc; i++) {
         jstring jstr = (jstring) (*env)->GetObjectArrayElement(env, commands, i);
         char* temp = (char*) (*env)->GetStringUTFChars(env, jstr, 0);
